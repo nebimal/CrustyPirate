c216528782b25ac106794d1476b4bcb94459f6fc
Nebi <maliknebi01@gmail.com>
2025-06-09 15:27:30 -0500

    Setup Player

--- Source/CrustyPirate/PlayerCharacter.cpp
+++ Source/CrustyPirate/PlayerCharacter.cpp
@@ -1,5 +1,61 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "PlayerCharacter.h"
 
+APlayerCharacter::APlayerCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(RootComponent);
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName); 
+}
+
+void APlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	APlayerController* PlayerController = Cast<APlayerController>(GetController());
+	if (PlayerController)
+	{
+		UEnhancedInputLocalPlayerSubsystem* Subsystem = 
+			ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer());
+		if (Subsystem)
+		{
+			Subsystem->AddMappingContext(InputMappingContext, 0);
+		}
+	}
+}
+void APlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) 
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+
+	if (EnhancedInputComponent)
+	{
+
+	}
+}
+
+void APlayerCharacter::Move(const FInputActionValue& Value)
+{
+
+}
+void APlayerCharacter::JumpStarted(const FInputActionValue& Value)
+{
+
+}
+void APlayerCharacter::JumpEnded(const FInputActionValue& Value)
+{
+
+}
+void APlayerCharacter::Attack(const FInputActionValue& Value)
+{
+
+}
--- Source/CrustyPirate/PlayerCharacter.h
+++ Source/CrustyPirate/PlayerCharacter.h
@@ -4,14 +4,53 @@
 
 #include "CoreMinimal.h"
 #include "PaperZDCharacter.h"
+
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Components/InputComponent.h"
+#include "InputActionValue.h"
+#include "GameFramework/Controller.h"
+
 #include "PlayerCharacter.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class CRUSTYPIRATE_API APlayerCharacter : public APaperZDCharacter
 {
 	GENERATED_BODY()
+
+public:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	USpringArmComponent* SpringArm;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	UCameraComponent* Camera;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UInputMappingContext* InputMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UInputAction* MoveAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UInputAction* JumpAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UInputAction* AttackAction;
+
+
+	APlayerCharacter();
+
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+
+	void Move(const FInputActionValue& Value);
+	void JumpStarted(const FInputActionValue& Value);
+	void JumpEnded(const FInputActionValue& Value);
+	void Attack(const FInputActionValue& Value);
 	
 };
