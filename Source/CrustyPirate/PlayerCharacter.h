2de137275eb9e63e831a58b320a4675e22c293dc
Nebi <maliknebi01@gmail.com>
2025-06-09 14:38:35 -0500

    Init

---
+++ Source/CrustyPirate/PlayerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperZDCharacter.h"
+#include "PlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CRUSTYPIRATE_API APlayerCharacter : public APaperZDCharacter
+{
+	GENERATED_BODY()
+	
+};
